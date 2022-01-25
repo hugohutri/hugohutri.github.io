851575f97cab32c8ca927afe53b13353f5f8e6ab
hugohutri <55588133+hugohutri@users.noreply.github.com>
2022-01-25 23:34:02 +0200

    fix: preview links now open new tab

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Link = styled.a.attrs({
+  target: "_blank",
   className:
     "text-blue-500 dark:text-blue-400 font-semibold underline hover:dark:text-blue-300 hover:text-blue-700",
 })``;
