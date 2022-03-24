ca7f50facbfc78fb607ad30b8d48cc671ba8d096
Hugo Hutri <55588133+hugohutri@users.noreply.github.com>
2022-03-24 20:39:14 +0200

    fix: change company

--- src/screens/About/components/AboutText.tsx
+++ src/screens/About/components/AboutText.tsx
@@ -5,8 +5,7 @@ export const AboutText = () => {
     <div className="lg:w-5/12 md:w-8/12 w-10/12 text-justify mx-auto text-2xl">
       <Text>
         As you probably noticed, I like web development and especially
-        front-&shy;end develop&shy;ment. I have been working at Visma Consulting
-        since 2020, and I have been constantly trying to learn more about web
+        front-&shy;end develop&shy;ment. I am currently working at Taito United, and I have been constantly trying to learn more about web
         development. I am currently finishing my master’s degree in
         soft&shy;ware engineering at LUT University, and it should be ready next
         winter.
